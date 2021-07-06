12dd078fcae065873fc36b7b6ee666e6fb8e9eec
alono1 <64021246+alono1@users.noreply.github.com>
2021-07-06 19:34:41 +0100

    Plots show trained data points

--- complete_model.py
+++ complete_model.py
@@ -13,7 +13,7 @@ import deepxde as dde # version 0.11
 from deepxde.backend import tf
 from create_plots import plot_1d
 
-# parse
+## parse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file-name', dest='file_name', required = True, type = str, help='File name for input data')
@@ -25,24 +25,24 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--plot', dest='plot', required = False, action='store_true', help='Create and save plots')
     args = parser.parse_args()
 
-# Network Parameters
-num_hidden_layer_1d = 3 # number of hidden layers for NN (1D)
-hidden_layer_size_1d = 20 # size of each hidden layers (1D)
-num_hidden_layer_2d = 4 # number of hidden layers for NN (2D)
+## Network Parameters
+num_hidden_layers_1d = 4 # number of hidden layers for NN (1D)
+hidden_layer_size_1d = 32 # size of each hidden layers (1D)
+num_hidden_layers_2d = 4 # number of hidden layers for NN (2D)
 hidden_layer_size_2d = 32 # size of each hidden layers (2D)
 num_domain = 10000 # number of training points within the domain
 num_boundary = 1000 # number of training boundary condition points on the geometry boundary
 num_test = 1000 # number of testing points within the domain
 
-# Training Parameters
-MAX_INIT = 10 # maximum number of times allowed to initialize the model
+## Training Parameters
+MAX_MODEL_INIT = 10 # maximum number of times allowed to initialize the model
 MAX_LOSS = 10 # upper limit to the initialized loss
 epochs = 50000 # number of epochs for training
 lr = 0.001 # learning rate
 noise = 0.1 # noise factor
-test_size = 0.2 # precentage of testing data
+test_size = 0.9 # precentage of testing data
 
-# PDE Parameters
+## PDE Parameters
 a = 0.01
 b = 0.15
 D = 0.1
@@ -51,7 +51,7 @@ mu_1 = 0.2
 mu_2 = 0.3
 epsilon = 0.002
 
-# Geometry Parameters
+## Geometry Parameters
 min_x = 0.1
 max_x = 10            
 min_y = 0.1
@@ -63,8 +63,8 @@ def params_to_inverse(a,b,D,param):
     params = []
     if not param:
         return a, b, D, params
-    # If inverse: 
-    # The tf.variables are initialized with a positive scalar, relatively close to their ground truth values
+    ## If inverse: 
+    ## The tf.variables are initialized with a positive scalar, relatively close to their ground truth values
     if 'a' in param:
         a = tf.math.exp(tf.Variable(-3.92))
         params.append(a)
@@ -104,7 +104,7 @@ def pde_1D(x, y):
     dv_dt = dde.grad.jacobian(y, x, i=0, j=1)
     dv_dxx = dde.grad.hessian(y, x, component=0, i=0, j=0)
     dw_dt = dde.grad.jacobian(y, x, i=1, j=1)
-    # Coupled PDE+ODE Equations
+    ## Coupled PDE+ODE Equations
     eq_a = dv_dt -  D*dv_dxx + k*V*(V-a)*(V-1) +W*V 
     eq_b = dw_dt -  (epsilon + (mu_1*W)/(mu_2+V))*(-W -k*V*(V-b-1))
     return [eq_a, eq_b]
@@ -116,7 +116,7 @@ def pde_2D(x, y):
     dv_dxx = dde.grad.hessian(y, x, component=0, i=0, j=0)
     dv_dyy = dde.grad.hessian(y, x, component=0, i=1, j=1)
     dw_dt = dde.grad.jacobian(y, x, i=1, j=2)
-    # Coupled PDE+ODE Equations
+    ## Coupled PDE+ODE Equations
     eq_a = dv_dt -  D*(dv_dxx + dv_dyy) + k*V*(V-a)*(V-1) +W*V 
     eq_b = dw_dt -  (epsilon + (mu_1*W)/(mu_2+V))*(-W -k*V*(V-b-1))
     return [eq_a, eq_b]
@@ -139,7 +139,7 @@ def pde_1D_2_cycle(x, y):
     I_stim = tf.ones_like(x_space)*0.1
     I_not_stim = tf.ones_like(x_space)*0
     Istim = tf.where(tf.logical_or(first_cond_stim,second_cond_stim),I_stim,I_not_stim)
-    # Coupled PDE+ODE Equations
+    ## Coupled PDE+ODE Equations
     eq_a = dv_dt -  D*dv_dxx + k*V*(V-a)*(V-1) +W*V -Istim
     eq_b = dw_dt -  (epsilon + (mu_1*W)/(mu_2+V))*(-W -k*V*(V-b-1))
     return [eq_a, eq_b]
@@ -163,28 +163,28 @@ def geometry_time(dim, observe_x):
 
 def main(args):
     
-    # Generate Data 
+    ## Generate Data 
     file_name = args.file_name
     observe_x, V, W = gen_data(file_name, args.dim)  
     
-    # Split data to train and test
+    ## Split data to train and test
     observe_train, observe_test, v_train, v_test, w_train, w_test = train_test_split(observe_x,V,W,test_size=test_size)
     
-    # Add noise to training data if needed
+    ## Add noise to training data if needed
     if args.noise:
         v_train = v_train + noise*np.random.randn(v_train.shape[0], v_train.shape[1])
 
-    # Define Initial Conditions
+    ## Define Initial Conditions
     T_ic = observe_train[:,-1].reshape(-1,1)
     idx_init = np.where(np.isclose(T_ic,1))[0]
     V_init = v_train[idx_init]
     observe_init = observe_train[idx_init]
     ic_1 = dde.PointSetBC(observe_init,V_init,component=0)
     
-    # Geometry and Time domains
+    ## Geometry and Time domains
     geomtime = geometry_time(args.dim, observe_x)
     
-    # Define Boundary Conditions
+    ## Define Boundary Conditions
     if args.dim == 1:
         bc_a = dde.NeumannBC(geomtime, lambda x:  np.zeros((len(x), 1)), lambda _, on_boundary: on_boundary, component=0)
     elif args.dim == 2:
@@ -193,21 +193,21 @@ def main(args):
     # Model observed data
     observe_v = dde.PointSetBC(observe_train, v_train, component=0)
     input_data = [bc_a, ic_1, observe_v]
-    # If W required as input
+    ## If W required as input
     if args.w_input:
         observe_w = dde.PointSetBC(observe_train, w_train, component=1)
         input_data = [bc_a, ic_1, observe_v, observe_w]
     
     if args.dim == 1:
-        # Select relevant PDE
+        ## Select relevant PDE
         pde = pde_1D
-        # Define the Network
-        net = dde.maps.FNN([2] + [hidden_layer_size_1d] * num_hidden_layer_1d + [2], "tanh", "Glorot uniform")
+        ## Define the Network
+        net = dde.maps.FNN([2] + [hidden_layer_size_1d] * num_hidden_layers_1d + [2], "tanh", "Glorot uniform")
     elif args.dim == 2:
-        # Select relevant PDE
+        ## Select relevant PDE
         pde = pde_2D
-        # Define the Network
-        net = dde.maps.FNN([3] + [hidden_layer_size_2d] * num_hidden_layer_2d + [2], "tanh", "Glorot uniform") 
+        ## Define the Network
+        net = dde.maps.FNN([3] + [hidden_layer_size_2d] * num_hidden_layers_2d + [2], "tanh", "Glorot uniform") 
     data = dde.data.TimePDE(geomtime, pde, input_data,
                             num_domain = num_domain, 
                             num_boundary=num_boundary, 
@@ -216,7 +216,7 @@ def main(args):
     model = dde.Model(data, net)
     model.compile("adam", lr=lr)
     
-    # Stabalize initialization process
+    ## Stabalize initialization process
     losshistory, _ = model.train(epochs=1)
     num_itr = len(losshistory.loss_train)
     init_loss = max(losshistory.loss_train[num_itr-1])
@@ -228,10 +228,10 @@ def main(args):
         losshistory, _ = model.train(epochs=1)
         num_itr = len(losshistory.loss_train)
         init_loss = max(losshistory.loss_train[num_itr-1])
-        if num_init > MAX_INIT:
-            exit()
+        if num_init > MAX_MODEL_INIT:
+            raise ValueError('Model initialization phases exceeded the allowed limit')
             
-    # Train Network
+    ## Train Network
     out_path = dir_path + args.model_folder_name
     if not args.inverse:
         losshistory, train_state = model.train(epochs=epochs, model_save_path = out_path)
@@ -241,17 +241,18 @@ def main(args):
         losshistory, train_state = model.train(epochs=epochs, model_save_path = out_path, callbacks=[variable])
     # dde.saveplot(losshistory, train_state, issave=True, isplot=True)
     
-    # Compute rMSE
+    ## Compute rMSE
     model_pred = model.predict(observe_test)
     v_pred = model_pred[:,0:1]
     rmse_v = np.sqrt(np.square(v_pred - v_test).mean())
     print('--------------------------')
     print("V rMSE test:", rmse_v)
     
-    # Plot   
+    # Plot
+    data_list = [observe_x, observe_train, V]
     if args.plot and args.dim == 1:
-        plot_1d(V, observe_x, model, args.model_folder_name)
+        plot_1d(data_list, model, args.model_folder_name)
     return train_state, v_pred, v_test 
 
-# Run main code
+## Run main code
 train_state, v_pred, v_test = main(args)
--- create_plots.py
+++ create_plots.py
@@ -3,53 +3,68 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_1d(v, observe_x, model, fig_name):
+def plot_1d(data_list, model, fig_name):
 
-    plot_1d_cell(v, observe_x, model, fig_name[1:-1])
-    plot_1d_array(v, observe_x, model, fig_name[1:-1])
+    plot_1d_cell(data_list, model, fig_name[1:-1])
+    plot_1d_array(data_list, model, fig_name[1:-1])
     return 0
     
-def plot_1d_cell(v, observe_x, model, fig_name):
+def plot_1d_cell(data_list, model, fig_name):
+    
+    # Unpack data
+    observe_x, observe_train, v = data_list[0], data_list[1], data_list[2]
     
     # Pick a random cell to show
-    obs_x = 6.0
+    obs_x = 15.0
+    
     # Get data for cell
     idx = [i for i,ix in enumerate(observe_x) if observe_x[i][0]==obs_x]
-    observe_test = observe_x[idx]
-    v_test = v[idx]
+    observe_geomtime = observe_x[idx]
+    v_observe = v[idx]
+    v_predict = model.predict(observe_geomtime)[:,0:1]
+    t_axis = observe_geomtime[:,1]
     
-    v_predict = model.predict(observe_test)[:,0:1]
-    t_axis = observe_test[:,1]
+    # Get data for points used in training process
+    idx_train = [i for i,ix in enumerate(observe_train) if observe_train[i][0]==obs_x]
+    t_markers = ((observe_train[idx_train])[:,1]).astype(int) - 1
+    
+    # create and save plot
     plt.figure()
-    
-    plt.plot(t_axis, v_test, c='r', label='observed')
-    plt.plot(t_axis, v_predict, c='b', label='predicted')
+    plt.plot(t_axis, v_observe, c='b', label='observed', marker='x', markevery=t_markers, ms=5)
+    plt.plot(t_axis, v_predict, c='r', label='predicted')
     plt.legend(loc='upper right')
     plt.xlabel('t')
     plt.ylabel('V')
+    plt.title('Potential vs Time: Cell Plot')
     plt.savefig(fig_name + "_cell_plot.png")
-    # plt.show()
     return 0
 
-def plot_1d_array(v, observe_x, model, fig_name):
+def plot_1d_array(data_list, model, fig_name):
+    
+    # Unpack data
+    observe_x, observe_train, v = data_list[0], data_list[1], data_list[2]
     
     # Pick a random point in time to show
     obs_t = 36.0
+    
     # Get all array data for chosen time 
     idx = [i for i,ix in enumerate(observe_x) if observe_x[i][1]==obs_t]
-    observe_test = observe_x[idx]
-    v_test = v[idx]
-    v_predict = model.predict(observe_test)[:,0:1]
-    x_ax = observe_test[:,0]
+    observe_geomtime = observe_x[idx]
+    v_observe = v[idx]
+    v_predict = model.predict(observe_geomtime)[:,0:1]
+    x_ax = observe_geomtime[:,0]
+    
+    # Get data for points used in training process
+    idx_train = [i for i,ix in enumerate(observe_train) if observe_train[i][1]==obs_t]
+    x_train = (((observe_train[idx_train])[:,0])*10).astype(int) -1
+
     # create and save plot
     plt.figure()
-    
-    plt.plot(x_ax, v_test, c='r', label='observed')
-    plt.plot(x_ax, v_predict, c='b', label='predicted')
+    plt.plot(x_ax, v_observe, c='b', label='observed',  marker='x', markevery=x_train, ms=5)
+    plt.plot(x_ax, v_predict, c='r', label='predicted')
     plt.legend(loc='upper left')
-    # plt.legend(loc='best')
     plt.xlabel('x')
     plt.ylabel('V')
+    plt.title('Potential vs Space: Cable Plot')
     plt.savefig(fig_name + "_array_plot.png")
-    # plt.show()
     return 0
