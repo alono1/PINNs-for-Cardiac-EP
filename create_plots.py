b0850e9ac3f823e35c739f9405dd8fa920a72890
alono1 <64021246+alono1@users.noreply.github.com>
2021-07-15 20:55:25 +0100

    save figures as TIFF

--- create_plots.py
+++ create_plots.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
-
+from PIL import Image
+import io
 
 def plot_1d(data_list, model, fig_name):
 
-    plot_1d_cell(data_list, model, fig_name[1:-1])
-    plot_1d_array(data_list, model, fig_name[1:-1])
+    plot_1d_cell(data_list, model, fig_name[1:])
+    plot_1d_array(data_list, model, fig_name[1:])
     return 0
     
 def plot_1d_cell(data_list, model, fig_name):
     
     ## Unpack data
-    observe_x, observe_train, v = data_list[0], data_list[1], data_list[2]
+    observe_x, observe_train, v_train, v = data_list[0], data_list[1], data_list[2], data_list[3]
     
     ## Pick a random cell to show
     obs_x = 15.0
@@ -20,33 +21,38 @@ def plot_1d_cell(data_list, model, fig_name):
     ## Get data for cell
     idx = [i for i,ix in enumerate(observe_x) if observe_x[i][0]==obs_x]
     observe_geomtime = observe_x[idx]
-    v_observe = v[idx]
+    v_GT = v[idx]
     v_predict = model.predict(observe_geomtime)[:,0:1]
     t_axis = observe_geomtime[:,1]
     
     ## Get data for points used in training process
     idx_train = [i for i,ix in enumerate(observe_train) if observe_train[i][0]==obs_x]
-    t_markers = ((observe_train[idx_train])[:,1]).astype(int) - 1
+    v_trained_points = v_train[idx_train]
+    t_markers = (observe_train[idx_train])[:,1]
     
-    ## create and save plot
+    ## create figure
     plt.figure()
     ## If there are any trained data points for the current cell 
     if len(t_markers):
-        plt.plot(t_axis, v_observe, c='b', label='observed', marker='x', markevery=t_markers, ms=5)
-    else:
-        plt.plot(t_axis, v_observe, c='b', label='observed')
-    plt.plot(t_axis, v_predict, c='r', label='predicted')
+        plt.scatter(t_markers, v_trained_points, marker='x', c='black',s=4, label='Observed')
+    plt.plot(t_axis, v_GT, c='b', label='GT')
+    plt.plot(t_axis, v_predict, c='r', label='Predicted')
     plt.legend(loc='upper right')
     plt.xlabel('t')
     plt.ylabel('V')
-    plt.title('Potential vs Time: Cell Plot')
-    plt.savefig(fig_name + "_cell_plot.png")
+    
+    ## save figure
+    png1 = io.BytesIO()
+    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    png2 = Image.open(png1)
+    png2.save(fig_name + "_cell_plot.tiff")
+    png1.close()
     return 0
 
 def plot_1d_array(data_list, model, fig_name):
     
     ## Unpack data
-    observe_x, observe_train, v = data_list[0], data_list[1], data_list[2]
+    observe_x, observe_train, v_train, v = data_list[0], data_list[1], data_list[2], data_list[3]
     
     ## Pick a random point in time to show
     obs_t = 36.0
@@ -54,25 +60,30 @@ def plot_1d_array(data_list, model, fig_name):
     ## Get all array data for chosen time 
     idx = [i for i,ix in enumerate(observe_x) if observe_x[i][1]==obs_t]
     observe_geomtime = observe_x[idx]
-    v_observe = v[idx]
+    v_GT = v[idx]
     v_predict = model.predict(observe_geomtime)[:,0:1]
     x_ax = observe_geomtime[:,0]
     
     ## Get data for points used in training process
     idx_train = [i for i,ix in enumerate(observe_train) if observe_train[i][1]==obs_t]
-    x_markers = (((observe_train[idx_train])[:,0])*10).astype(int) -1
+    v_trained_points = v_train[idx_train]
+    x_markers = (observe_train[idx_train])[:,0]
 
-    ## create and save plot
+    ## create figure
     plt.figure()
     ## If there are any trained data points for the current time step
     if len(x_markers):
-        plt.plot(x_ax, v_observe, c='b', label='observed',  marker='x', markevery=x_markers, ms=5)
-    else:
-        plt.plot(x_ax, v_observe, c='b', label='observed')
-    plt.plot(x_ax, v_predict, c='r', label='predicted')
+        plt.scatter(x_markers, v_trained_points, marker='x', c='black',s=4, label='Observed')
+    plt.plot(x_ax, v_GT, c='b', label='GT')
+    plt.plot(x_ax, v_predict, c='r', label='Predicted')
     plt.legend(loc='upper left')
     plt.xlabel('x')
     plt.ylabel('V')
-    plt.title('Potential vs Space: Cable Plot')
-    plt.savefig(fig_name + "_array_plot.png")
+    
+    ## save figure
+    png1 = io.BytesIO()
+    plt.savefig(png1, format="png", dpi=500, pad_inches = .1, bbox_inches = 'tight')
+    png2 = Image.open(png1)
+    png2.save(fig_name + "_array_plot.tiff")
+    png1.close()
     return 0
